d460469a7894e3e8286c8ef68e5140357bee52db
dungbangj <yoyob1223@gmail.com>
2022-08-23 17:06:24 +0900

    at home

---
+++ ft_printf/withBonus/src/total.c
@@ -0,0 +1,333 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   total.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoyob1223 <yoyob1223@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 20:58:05 by yback             #+#    #+#             */
+/*   Updated: 2022/08/23 16:15:25 by yoyob1223        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+typedef struct info_s
+{
+	int		zeropad;
+	char	sign;
+	int		plus;
+	int		space;
+	int		left;
+	int		small;
+	int		special;
+	int		width;
+	int		precision;
+	char	*str;
+	int		base;
+	char	fmt;
+}	info_t;
+
+int	ft_atoi(const char **fmt)
+{
+	int		i;
+
+	i = 0;
+	while (**fmt >= '0' && **fmt <= '9')
+		i = i * 10 + *((*fmt)++) - '0';
+	return (i);
+}
+
+int	ft_div(long *n, int base)
+{
+	int	res;
+
+	res = (unsigned long)*n % (unsigned int)base;
+	*n = (unsigned long)*n / (unsigned int)base;
+	return (res);
+}
+
+int	ft_strnlen(char *s, int max)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && i < max)
+		i++;
+	return (i);
+}
+
+void	init_info(info_t *info)
+{
+	info->base = 10;
+	info->left = -1;
+	info->plus = -1;
+	info->precision = -1;
+	info->sign = 0;
+	info->small = -1;
+	info->space = -1;
+	info->special = -1;
+	info->width = -1;
+	info->zeropad = -1;
+	info->fmt = 0;
+}
+
+char	*number(info_t *info, long num, char *wow)
+{
+	char	digits[16] = "0123456789ABCDEF";
+	char	newarr[100];
+	char	c;
+	int		i;
+	int		tmp;
+
+	tmp = num;
+	if (info->left == 1)
+		info->zeropad = 1;
+	if (info->base < 2 || info->base > 16)
+		return (NULL);
+	if (info->zeropad == 1)
+		c = '0';
+	else if (info->zeropad == -1)
+		c = ' ';	
+	if (info->sign == 1)
+	{
+		if (num < 0)
+		{
+			info->sign = '-';
+			num *= -1;
+			info->width--;
+		}
+		else if (info->plus == 1)
+		{
+			info->sign = '+';
+			info->width--;
+		}
+		else if (info->space == 1)
+		{
+			info->sign = ' ';
+			info->width--;
+		}
+	}
+	if (info->special == 1)
+	{
+		if (info->base == 16)
+			info->width -= 2;
+	}
+	i = 0;
+	if (num == 0 && info->precision == 0)
+		return (wow);
+	else if (num == 0)
+		newarr[i++] = '0';
+	else
+		while (num != 0)
+		{
+			if (info->small == 1)
+			{
+				newarr[i] = digits[ft_div(&num, info->base)];
+				if (newarr[i] >= 'A' && newarr[i] <= 'F')
+					newarr[i] += ('a' - 'A');
+				i++;
+			}
+			else
+				newarr[i++] = digits[ft_div(&num, info->base)];
+		}	
+	if (i > info->precision)
+		info->precision = i;
+	info->width -= info->precision;
+	if (info->zeropad != 1 && info->left != 1)
+		while (info->width > 0)
+		{
+			*wow++ = ' ';
+			info->width--;
+		}
+	if (info->sign)
+		*wow++ = info->sign;
+	if (info->special == 1)
+	{
+		if (info->base == 16 && !((info->fmt == 'x' || info->fmt == 'X') && tmp == 0))
+		{
+			*wow++ = '0';
+			if (info->small == 1)
+				*wow++ = 'x';
+			else
+				*wow++ = 'X';
+		}
+	}
+	if (info->left != 1)
+		while (info->width > 0)
+		{
+			info->width--;
+			*wow++ = c;
+		}
+	while (i < info->precision--)
+		*wow++ = '0';
+	while (i > 0)
+		*wow++ = newarr[--i];
+	while (info->width > 0)
+	{
+		info->width--;
+		*wow++ = ' ';
+	}
+	return (wow);
+}
+
+int	ft_vsprintf(char *buf, const char *fmt, va_list ap, info_t *info)
+{
+	int				len;
+	unsigned long	num;
+	char			*s;
+	int				i;
+	char			*wow;
+	
+	wow = buf;
+	while (*fmt)
+	{
+		init_info(info);
+		if (*fmt != '%')
+		{
+			*wow++ = *fmt++;
+			continue;
+		}
+		++fmt;
+		while (*fmt == '-' || *fmt == '+' || *fmt == ' ' || *fmt == '#' || *fmt == '0')
+		{
+			if (*fmt == '-')
+				info->left = 1;
+			else if (*fmt == '+')
+				info->plus = 1;
+			else if (*fmt == ' ')
+				info->space = 1;
+			else if (*fmt == '#')
+				info->special = 1;
+			else if (*fmt == '0')
+				info->zeropad = 1;
+			++fmt;
+		}
+		if (*fmt >= '0' && *fmt <= '9')
+			info->width = ft_atoi(&fmt);
+		if (*fmt == '.')
+		{
+			fmt++;
+			if (*fmt >= '0' && *fmt <= '9')
+				info->precision = ft_atoi(&fmt);
+			if (info->precision < 0)
+				info->precision = 0;
+		}
+		info->fmt = *fmt;
+		if (*fmt == 'c')
+		{
+			if (info->left == -1)
+			{
+				info->width--;
+				while (info->width > 0)
+				{
+					*wow++ = ' ';
+					info->width--;
+				}
+			}
+			*wow++ = (unsigned char)va_arg(ap, int);
+			info->width--;
+			while (info->width > 0)
+			{
+				*wow++ = ' ';
+				info->width--;
+			}
+			fmt++;
+			continue;
+		}
+		else if (*fmt == 's')
+		{
+			s = va_arg(ap, char *);
+			len = ft_strnlen(s, info->precision);
+			if (info->left == -1)
+			{
+				while (len < info->width)
+				{
+					*wow++ = ' ';
+					info->width--;
+				}
+			}
+			while (i < len)
+			{
+				*wow++ = *s++;
+				i++;
+			}
+			while (len < info->width)
+			{
+				*wow++ = ' ';
+				info->width--;
+			}
+			fmt++;
+			continue;
+		}
+		else if (*fmt == 'p')
+		{
+			info->small = 1;
+			info->special = 1;
+			info->base = 16;
+			wow = number(info, (unsigned long)va_arg(ap, void *), wow);
+			fmt++;
+			continue;
+		}
+		else if (*fmt == '%')
+		{
+			*wow++ = '%';
+			fmt++;
+			continue;
+		}
+		else if (*fmt == 'x')
+		{
+			info->small = 1;
+			info->base = 16;
+		}
+		else if (*fmt == 'X')
+			info->base = 16;
+		else if (*fmt == 'd' || *fmt == 'i')
+			info->sign = 1;
+		else if (*fmt != 'u')
+		{
+			*wow++ = '%';
+			if (*fmt)
+				*wow++ = *fmt;
+			else
+				--fmt;
+			fmt++;
+			continue;
+		}
+		if (info->sign == 1)
+			num = va_arg(ap, int);
+		else
+			num = va_arg(ap, unsigned int);		
+		wow = number(info, num, wow);
+		fmt++;
+	}
+	*wow = '\0';
+	return (wow - buf);
+}
+
+int ft_printf(const char *fmt, ...)
+{
+	char	arr[1024];
+	va_list	ap;
+	info_t	info;
+	int		i;
+
+	va_start(ap, fmt);
+	i = ft_vsprintf(arr, fmt, ap, &info);
+	va_end(ap);
+
+	write(1, arr, i);
+	return (i);
+}
+
+#define A "  %u %#x %#X %c\n"
+#define B  -2147483648, -2147483648, -2147483648, 97
+
+int main(void)
+{
+	printf("length of printf = %d\n\n", printf(A, B));
+	printf("length of ft_printf = %d\n\n", ft_printf(A, B));
+}
