afc12867b5feabd3700e5cf2734c62671dd0035d
yback1223 <yoyob1223@gmail.com>
2022-08-29 21:44:01 +0900

    good today

--- ft_printf/withBonus/src/ft_printf.c
+++ ft_printf/withBonus/src/ft_printf.c
@@ -5,96 +5,55 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yback <yback@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/25 19:58:05 by yback             #+#    #+#             */
-/*   Updated: 2022/08/25 20:57:04 by yback            ###   ########.fr       */
+/*   Created: 2022/08/17 18:13:38 by yback             #+#    #+#             */
+/*   Updated: 2022/08/18 10:15:36 by yback            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdlib.h>
-#include <unistd.h>
 
-char	*number(t_info *info, char *wow)
+int	ft_whichformat(va_list ap, char c)
 {
-	char	sign;
-	char	*newarr;
-	int		i;
-	long	tmp;
-	int		w;
+	int	total_length;
 
-	w = info->width;
-	i = 0;
-	tmp = info->num;
-	sign = 0;
-	if (info->left == 1)
-		info->zeropad = -1;
-	if (info->base < 2 || info->base > 16)
-		return (NULL);
-	making_sign_and_spe(info, &sign, tmp);
-	newarr = making_newarr(info, &i);
-	wow = making_setting_one(info, &i, wow);
-	if (info->left != 1 && info->zeropad == 1 && info->precisionstat == -1)
-		wow = making_setting_two(info, sign, wow, tmp);
-	else if (info->left != 1 && info->zeropad == 1 && w > info->precision)
-		wow = making_setting_three(info, wow, sign, tmp);
-	else
-		wow = making_setting_four(info, wow, sign, tmp);
-	wow = finish_part(info, i, wow, newarr);
-	return (wow);
+	total_length = 0;
+	if (c == 'c')
+		total_length += ft_for_c(va_arg(ap, int));
+	else if (c == 's')
+		total_length += ft_for_s(va_arg(ap, char *));
+	else if (c == 'p')
+		total_length += ft_for_p(va_arg(ap, unsigned long long));
+	else if (c == 'd' || c == 'i')
+		total_length += ft_for_d(va_arg(ap, int));
+	else if (c == 'u')
+		total_length += ft_for_u(va_arg(ap, unsigned int));
+	else if (c == 'x' || c == 'X')
+		total_length += ft_for_xx(va_arg(ap, unsigned int), c);
+	else if (c == '%')
+		total_length += ft_for_percent();
+	return (total_length);
 }
 
-char	*ft_spec_intx(t_info *info, const char **fmt, va_list ap, char *wow)
+int	ft_printf(const char *arr, ...)
 {
-	if (**fmt == 'c' || **fmt == 's')
-		wow = ft_for_sc(info, &(*fmt), wow, ap);
-	else if (**fmt == 'p' || **fmt == '%' || **fmt != 'u')
-		wow = ft_w_con(info, &(*fmt), wow, ap);
-	(*fmt)++;
-	return (wow);
-}
-
-int	ft_main_pri(char *buf, const char *fmt, va_list ap, t_info *info)
-{
-	char	*wow;
-
-	wow = buf;
-	while (*fmt)
-	{
-		if (*fmt != '%' && *fmt)
-		{
-			*wow++ = *fmt++;
-			continue ;
-		}
-		init_info(info, &fmt);
-		ft_for_flags(info, &fmt);
-		if (*fmt == 'x' || *fmt == 'X' || *fmt == 'd'
-			|| *fmt == 'i' || *fmt == 'u')
-			wow = ft_wo_con(info, &fmt, ap, wow);
-		else
-		{
-			wow = ft_spec_intx(info, &fmt, ap, wow);
-			continue ;
-		}
-		fmt++;
-	}
-	*wow = '\0';
-	return (wow - buf);
-}
-
-int	ft_printf(const char *fmt, ...)
-{
-	char	arr[1000000];
 	va_list	ap;
-	t_info	info;
+	int		total_length;
 	int		i;
-	int		j;
 
-	j = 0;
 	i = 0;
-	va_start(ap, fmt);
-	i = ft_main_pri(arr, fmt, ap, &info);
+	total_length = 0;
+	va_start(ap, arr);
+	while (arr[i])
+	{
+		if (arr[i] == '%')
+		{
+			total_length += ft_whichformat(ap, arr[i + 1]);
+			i++;
+		}
+		else
+			total_length += ft_for_c(arr[i]);
+		i++;
+	}
 	va_end(ap);
-	while (j < i)
-		write(1, &arr[j++], 1);
-	return (i);
+	return (total_length);
 }
