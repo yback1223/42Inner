b230a0ffac3b87d83ae2fb883f850fb41a3b1162
yback1223 <yoyob1223@gmail.com>
2023-01-30 20:29:48 +0900

    doing

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yback <yback@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 21:17:36 by yback             #+#    #+#             */
-/*   Updated: 2023/01/30 18:32:58 by yback            ###   ########.fr       */
+/*   Updated: 2023/01/30 20:10:41 by yback            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,8 @@ void	error(void);
 char	*get_path(char *command, char **envp);
 void	execute(char *av, char **envp);
 
+/* Bonus functions */
+int		get_next_line(char **line);
+int		open_file(char *argv, int i);
 
 #endif
--- pipex/srcs/pipex_bonus_utils.c
+++ pipex/srcs/pipex_bonus_utils.c
@@ -6,7 +6,7 @@
 /*   By: yback <yback@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 19:37:11 by yback             #+#    #+#             */
-/*   Updated: 2023/01/30 19:37:45 by yback            ###   ########.fr       */
+/*   Updated: 2023/01/30 20:03:46 by yback            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,31 @@ int	open_file(char *argv, int i)
 	if (file == -1)
 		error();
 	return (file);
+}
+
+int	get_next_line(char **line)
+{
+	char	*buffer;
+	int		i;
+	int		r;
+	char	c;
+
+	i = 0;
+	r = 0;
+	buffer = (char *)malloc(10000);
+	if (!buffer)
+		return (-1);
+	r = read(0, &c, 1);
+	while (r && c != '\n' && c != '\0')
+	{
+		if (c != '\n' && c != '\0')
+			buffer[i] = c;
+		i++;
+		r = read(0, &c, 1);
+	}
+	buffer[i] = '\n';
+	buffer[++i] = '\0';
+	*line = buffer;
+	free(buffer);
+	return (r);
 }
