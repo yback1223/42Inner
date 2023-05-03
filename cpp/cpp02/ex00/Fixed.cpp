4136dc997d4fb3a2552b1bbef34aad024ae95ac0
yback1223 <yoyob1223@gmail.com>
2023-05-03 19:20:11 +0900

    Add headers

--- cpp/cpp02/ex00/Fixed.cpp
+++ cpp/cpp02/ex00/Fixed.cpp
@@ -1,6 +1,14 @@
-//
-// Created by Yong Min Back on 2023/04/30.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yback <yback@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 19:09:37 by yback             #+#    #+#             */
+/*   Updated: 2023/05/03 19:11:36 by yback            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "Fixed.h"
 
@@ -13,28 +21,23 @@ Fixed::Fixed(const Fixed &copy) {
 	*this = copy;
 }
 
-Fixed::~Fixed()
-{
+Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits() const
-{
+int Fixed::getRawBits() const {
 	std::cout << "getRawBits member function called" << std::endl;
 	return this->val;
 }
 
-void Fixed::setRawBits(const int raw)
-{
+void Fixed::setRawBits(const int raw) {
 	std::cout << "setRawBits member function called" << std::endl;
 	this->val = raw;
 }
 
-Fixed &Fixed::operator=(const Fixed &copy)
-{
+Fixed &Fixed::operator=(const Fixed &copy) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &copy)
-	{
+	if (this != &copy) {
 		this->val = copy.getRawBits();
 	}
 	return *this;
