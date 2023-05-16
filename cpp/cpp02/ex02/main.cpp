ad445f9ed67005dac2f732fefe903b62060ca53a
yback1223 <yoyob1223@gmail.com>
2023-05-16 17:53:47 +0900

    finish cpp02

--- cpp/cpp02/ex02/main.cpp
+++ cpp/cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yback <yback@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 19:10:27 by yback             #+#    #+#             */
-/*   Updated: 2023/05/16 14:46:06 by yback            ###   ########seoul.kr  */
+/*   Updated: 2023/05/16 17:47:09 by yback            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 #include <iostream>
 
 int main( void ) {
-    Fixed a;
+	{
+		Fixed a;
+		Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+		std::cout << "===MAIN===" << std::endl;
+		std::cout << a << std::endl;
+		std::cout << ++a << std::endl;
+		std::cout << a << std::endl;
+		std::cout << a++ << std::endl;
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << Fixed::max( a, b ) << std::endl;
+	}
+    
+	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-
-	std::cout << "===MAIN===" << std::endl;
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-
-
+	
     std::cout << "a = " << a << std::endl;
     std::cout << "b = " << b << std::endl;
 
@@ -41,7 +45,7 @@ int main( void ) {
     std::cout << "a - b\n" << (a - b) << std::endl;
     std::cout << "a * b\n" << (a * b) << std::endl;
     std::cout << "a / b\n" << (a / b) << std::endl;
-  	// std::cout << "b / a\n" << (b / a) << std::endl;
+  	std::cout << "b / a\n" << (b / a) << std::endl;
 
     std::cout << "\n===Prefix/Postfix operator===" << std::endl;
     std::cout << "a = " << a << std::endl;
@@ -55,6 +59,5 @@ int main( void ) {
     std::cout << "a-- = " << a-- << std::endl;
     std::cout << "--a = " << --a << std::endl;
     std::cout << "--a = " << --a << std::endl;
-
 	return 0;
 }
